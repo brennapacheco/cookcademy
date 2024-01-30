f1f03c87da95ffdf19b39d991deb261b19e5132c
Brenna Pacheco <brennapacheco@hotmail.com>
2024-01-30 10:56:32 +0000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Brenna Pacheco on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
