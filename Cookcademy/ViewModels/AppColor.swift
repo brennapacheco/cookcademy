401cecbdff5681c3efb35ad0a3785e5a8374fb75
Brenna Pacheco <brennapacheco@hotmail.com>
2024-01-31 17:03:34 +0000

    Added AppColor

--- Cookcademy/ViewModels/AppColor.swift
+++ Cookcademy/ViewModels/AppColor.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AppColor {
+    static let background: Color = Color(.sRGB,
+                                         red: 228/255,
+                                         green: 235/255,
+                                         blue: 250/255,
+                                         opacity: 1)
+    static let foreground: Color = Color(.sRGB,
+                                         red: 118/255,
+                                         green: 119/255,
+                                         blue: 231/255,
+                                         opacity: 1)
+}
