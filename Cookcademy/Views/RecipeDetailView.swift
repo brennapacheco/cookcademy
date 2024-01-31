35b57377227c5e10d6c9bf012d4060e4e8facf56
Brenna Pacheco <brennapacheco@hotmail.com>
2024-01-31 17:07:16 +0000

    Create RecipeDetailView

--- Cookcademy/Views/RecipeDetailView.swift
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
+    let recipe: Recipe
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Author: \(recipe.mainInformation.author)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            
+            HStack {
+                Text(recipe.mainInformation.description)
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            
+            List {
+                
+                Section(header: Text("Ingredients")) {
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                        let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                            .foregroundColor(listTextColor)
+                    }
+                }.listRowBackground(listBackgroundColor)
+                
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let direction = recipe.directions[index]
+                        HStack {
+                            Text("\(index + 1).").bold()
+                            Text("\(direction.isOptional ? "(Optional)" : "")" + direction.description)
+                            
+                        }.foregroundColor(listTextColor)
+                    }
+                }
+                .listRowBackground(listBackgroundColor)
+            }
+        }
+        .navigationTitle(recipe.mainInformation.name)
     }
 }
 
 #Preview {
-    RecipeDetailView()
+    RecipeDetailView(recipe: Recipe.testRecipes[0])
 }
