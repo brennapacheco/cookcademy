bbced229a0b17008b868866ff3170d621c75019e
Brenna Pacheco <brennapacheco@hotmail.com>
2024-02-02 13:59:41 +0000

    Create a method to filter a category and return a recipe

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -9,4 +9,14 @@ import Foundation
 
 class RecipeData: ObservableObject {
   @Published var recipes = Recipe.testRecipes
+    
+    func recipes(for category: MainInformation.Category) -> [Recipe] {
+        var filteredRecipes = [Recipe]()
+        for recipe in recipes {
+          if recipe.mainInformation.category == category {
+            filteredRecipes.append(recipe)
+          }
+        }
+        return filteredRecipes
+      }
 }
