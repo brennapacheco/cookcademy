d38f1f220db7b88c9eee4a5c7b90b839cbc7b6c9
Brenna Pacheco <brennapacheco@hotmail.com>
2024-02-19 23:17:30 +0000

    Added a recipe validation

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -23,6 +23,10 @@ struct Recipe: Identifiable {
     init() {
         self.init(mainInformation: MainInformation(name: "", description: "", author: "", category: .breakfast), ingredients: [], directions: [])
     }
+    
+    var isValid: Bool {
+        mainInformation.isValid && !ingredients.isEmpty && !directions.isEmpty
+      }
 }
 
 struct MainInformation {
@@ -37,6 +41,10 @@ struct MainInformation {
         case dinner = "Dinner"
         case dessert = "Dessert"
     }
+    
+    var isValid: Bool {
+        !name.isEmpty && !description.isEmpty && !author.isEmpty
+      }
 }
 
 struct Ingredient {
---
+++ Cookcademy/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyRecipeView.swift
+//  Cookcademy
+//
+//  Created by Brenna Pacheco on 02/02/24.
+//
+
+import SwiftUI
+
+struct ModifyRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyRecipeView()
+}
