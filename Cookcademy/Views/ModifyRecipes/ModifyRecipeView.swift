7ba49dd84ba00bd80f76f92072fed60e6fac1fea
Brenna Pacheco <brennapacheco@hotmail.com>
2024-02-20 16:59:54 +0000

    Added a picker to ModifyRecipeView

--- Cookcademy/Views/ModifyRecipes/ModifyRecipeView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -10,14 +10,33 @@ import SwiftUI
 struct ModifyRecipeView: View {
     @Binding var recipe: Recipe
     
+    @State private var selection = Selection.main
+    
     var body: some View {
         VStack {
-            Button("Fill in the recipe with test data.") {
-                recipe.mainInformation = MainInformation(name: "test", description: "test", author: "test", category: .breakfast)
-                recipe.directions = [Direction(description: "test", isOptional: false)]
-                recipe.ingredients = [Ingredient(name: "test", quantity: 1.0, unit: .none)]
+            Picker("Picker", selection: $selection, content: {
+                Text("Main Info").tag(Selection.main)
+                Text("Ingredients").tag(Selection.ingredients)
+                Text("Directions").tag(Selection.directions)
+            })
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            switch selection {
+            case .main:
+                Text("Main Editor")
+            case .ingredients:
+                Text("Ingredients Editor")
+            case .directions:
+                Text("Directions Editor")
             }
+            Spacer()
         }
+    }
+}
+
+extension ModifyRecipeView {
+    enum Selection {
+        case main, ingredients, directions
     }
 }
 
