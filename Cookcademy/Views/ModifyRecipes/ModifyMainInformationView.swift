c9e7da2aa33a25512fc03b6a21ecc809e3a9ddbd
Brenna Pacheco <brennapacheco@hotmail.com>
2024-02-20 17:39:04 +0000

    Set color palette to ModifyMainInformationView

--- Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -8,32 +8,40 @@
 import SwiftUI
 
 struct ModifyMainInformationView: View {
-  @Binding var mainInformation: MainInformation
-  
-  var body: some View {
-    Form {
-      TextField("Recipe Name", text: $mainInformation.name)
-      TextField("Author", text: $mainInformation.author)
-      Section(header: Text("Description")) {
-        TextEditor(text: $mainInformation.description)
-      }
-      Picker(selection: $mainInformation.category, label:
-              HStack {
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    @Binding var mainInformation: MainInformation
+    
+    var body: some View {
+        Form {
+            TextField("Recipe Name", text: $mainInformation.name)
+                .listRowBackground(listBackgroundColor)
+            TextField("Author", text: $mainInformation.author)
+                .listRowBackground(listBackgroundColor)
+            Section(header: Text("Description")) {
+                TextEditor(text: $mainInformation.description)
+                    .listRowBackground(listBackgroundColor)
+            }
+            Picker(selection: $mainInformation.category, label:
+                    HStack {
                 Text("Category")
                 Spacer()
-              }) {
-        ForEach(MainInformation.Category.allCases,
-                id: \.self) { category in
-          Text(category.rawValue)
+            }) {
+                ForEach(MainInformation.Category.allCases,
+                        id: \.self) { category in
+                    Text(category.rawValue)
+                }
+            }
+            .listRowBackground(listBackgroundColor)
+            .pickerStyle(MenuPickerStyle())
         }
-              }
-      .pickerStyle(MenuPickerStyle())
+        .foregroundStyle(listTextColor)
     }
-  }
 }
 
 #Preview {
-    @State var mainInfo = MainInformation(name: "Test Name", 
+    @State var mainInfo = MainInformation(name: "Test Name",
                                           description: "Test description",
                                           author: "Test author",
                                           category: .lunch)
