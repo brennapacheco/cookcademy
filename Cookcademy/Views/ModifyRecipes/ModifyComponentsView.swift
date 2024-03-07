dfd788384f512862055563432ae2ab15e703423b
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-07 14:39:00 +0000

    Added a view to modify individual components

--- Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
@@ -56,12 +56,13 @@ struct ModifyComponentsView<Component: RecipeComponent, DestinationView: ModifyC
                 List {
                     ForEach(components.indices, id: \.self) { index in
                         let component = components[index]
-                        Text(component.description)
+                        let editComponentView = DestinationView(component: $components[index]) { _ in
+                            return
+                        }
+                            .navigationTitle("Edit \(Component.singularName().capitalized)")
+                        NavigationLink(component.description,
+                                       destination: editComponentView)
                     }
-                    .listRowBackground(listBackgroundColor)
-                    NavigationLink("Add another \(Component.singularName())",
-                                   destination: addComponentView)
-                    .buttonStyle(PlainButtonStyle())
                     .listRowBackground(listBackgroundColor)
                 }
                 .foregroundStyle(listTextColor)
