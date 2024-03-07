6691379037182e9f62a831f8203a3051de3e6c00
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-07 13:31:33 +0000

    Added a binding to RecipeData index method in RecipesListView

--- Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -72,6 +72,11 @@ extension RecipesListView {
     private var navigationTitle: String {
         "\(category.rawValue) Recipes"
     }
+    
+    func binding(for recipe: Recipe) -> Binding<Recipe> {
+        guard let index = recipeData.index(of: recipe) else { fatalError("Recipe not found") }
+        return $recipeData.recipes[index]
+    }
 }
 
 #Preview {
