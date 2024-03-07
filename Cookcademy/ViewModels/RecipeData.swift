3909e387fb83ba5e747ba019c9d53d5af1ae82c6
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-07 13:10:58 +0000

    Added a method that returns the index of a given recipe and fix indentation

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -8,21 +8,30 @@
 import Foundation
 
 class RecipeData: ObservableObject {
-  @Published var recipes = Recipe.testRecipes
+    @Published var recipes = Recipe.testRecipes
     
     func recipes(for category: MainInformation.Category) -> [Recipe] {
         var filteredRecipes = [Recipe]()
         for recipe in recipes {
-          if recipe.mainInformation.category == category {
-            filteredRecipes.append(recipe)
-          }
+            if recipe.mainInformation.category == category {
+                filteredRecipes.append(recipe)
+            }
         }
         return filteredRecipes
-      }
+    }
     
     func add(recipe: Recipe) {
         if recipe.isValid {
-          recipes.append(recipe)
+            recipes.append(recipe)
         }
-      }
+    }
+    
+    func index(of recipe: Recipe) -> Int? {
+        for i in recipes.indices {
+            if recipes[i].id == recipe.id {
+                return i
+            }
+        }
+        return nil
+    }
 }
