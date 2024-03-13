abdf08c5f2c806ce79a8d16cca04f4e523f4656f
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-13 18:29:06 +0000

    Created a Favorite Button in the Recipe Detail View

--- Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -59,6 +59,12 @@ struct RecipeDetailView: View {
                     Button("Edit") {
                         isPresenting = true
                     }
+                    Button {
+                        recipe.isFavorite.toggle()
+                    } label: {
+                        Image(systemName: recipe.isFavorite ? "heart.fill" : "heart")
+                    }
+
                 }
             }
         }
