1909ef9221dfb1be80ee65232b2200ad7dffb079
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-25 18:25:35 +0000

    Created saveRecipes and loadRecipes methods to persist recipes

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -14,6 +14,15 @@ class RecipeData: ObservableObject {
         recipes.filter { $0.isFavorite }
     }
     
+    private var recipesFileURL: URL {
+        do {
+            let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            return documentsDirectory.appending(path: "recipeData")
+        } catch {
+            fatalError("An error occurred while getting the url: \(error)")
+        }
+    }
+    
     func recipes(for category: MainInformation.Category) -> [Recipe] {
         var filteredRecipes = [Recipe]()
         for recipe in recipes {
@@ -37,5 +46,25 @@ class RecipeData: ObservableObject {
             }
         }
         return nil
+    }
+    
+    func saveRecipes() throws {
+        do {
+            let encodedData = try JSONEncoder().encode(recipes)
+            try encodedData.write(to: recipesFileURL)
+        } catch {
+            fatalError("An error occurred while getting the url: \(error)")
+        }
+    }
+    
+    func loadRecipes() throws {
+        guard let data = try? Data(contentsOf: recipesFileURL) else { return }
+
+        do {
+            let savedRecipes = try JSONDecoder().decode([Recipe].self, from: data)
+            recipes = savedRecipes
+        } catch {
+            fatalError("An error occurred while getting the url: \(error)")
+        }
     }
 }
