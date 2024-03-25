bab09d3cb568411e97f805af14b8a9ec242f4f97
Brenna Pacheco <brennapacheco@hotmail.com>
2024-03-25 17:46:04 +0000

    Added codable conformance to Recipe

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Recipe: Identifiable {
+struct Recipe: Identifiable, Codable {
     var id = UUID()
     
     var mainInformation: MainInformation
@@ -36,13 +36,13 @@ struct Recipe: Identifiable {
     }
 }
 
-struct MainInformation {
+struct MainInformation: Codable {
     var name: String
     var description: String
     var author: String
     var category: Category
     
-    enum Category: String, CaseIterable {
+    enum Category: String, CaseIterable, Codable {
         case breakfast = "Breakfast"
         case lunch = "Lunch"
         case dinner = "Dinner"
@@ -74,7 +74,7 @@ struct Ingredient: RecipeComponent {
         }
     }
     
-    enum Unit: String, CaseIterable {
+    enum Unit: String, CaseIterable, Codable {
         case oz = "Ounces"
         case g = "Grams"
         case cups = "Cups"
